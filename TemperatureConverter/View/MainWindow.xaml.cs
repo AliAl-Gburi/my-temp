93cf501748ec94e6f184651e8a6303750da8cb9c
AliAl-Gburi <78793598+AliAl-Gburi@users.noreply.github.com>
2022-03-18 03:53:51 +0100

    temporary3

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -19,6 +19,46 @@ namespace View
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+    public class FahrenheitConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var kelvin = (double)value;
+            var fahrenheit = 1.8 * (kelvin - 273.15) + 32;
+
+            return fahrenheit.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var fahrenheit = double.Parse((string)value);
+            var kelvin = (fahrenheit + 459.67) * (5 / 9);
+
+            return kelvin;
+        }
+    }
+    public class CelsiusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var kelvin = (double)value;
+            var celsius = kelvin - 273.15;
+
+            return celsius.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var celsius = double.Parse((string)value);
+            var kelvin = celsius + 273.15;
+
+            return kelvin;
+        }
+    }
+
+   
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -26,36 +66,6 @@ namespace View
             InitializeComponent();
         }
 
-        public class CelsiusConverter : IValueConverter
-        {
-            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-            {
-                var kelvin = (double)value;
-                var celsius = kelvin - 273.15;
-
-                return celsius.ToString();
-            }
-
-            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-            {
-                var celsius = double.Parse((string)value);
-                var kelvin = celsius + 273.15;
-
-                return kelvin;
-            }
-        }
-
-
-
-        private void SliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            var kDouble = slider.Value;
-            
-
-            
-
-            
-
-        }
+        
     }
 }
